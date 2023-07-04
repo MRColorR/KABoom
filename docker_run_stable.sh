e8af2bcbdea2b3b85411425d6dcd94b134149f43
MRColor <mattiar11@gmail.com>
2023-07-04 19:38:38 +0200

    CHG - moved stable to port 8000

--- docker_run_stable.sh
+++ docker_run_stable.sh
@@ -4,4 +4,4 @@
 # docker run --name abeweb_stable -p 8000:8000 -d abe-webapp
 
 docker build -t mrcolorrain/kaboom:latest .\abe-webapp\ --no-cache
-docker run --name kaboom_abeweb_stable -p 8001:8000 -d mrcolorrain/kaboom:latest
+docker run --name kaboom_abeweb_stable -p 8000:8000 -d mrcolorrain/kaboom:latest

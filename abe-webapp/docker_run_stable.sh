ffcbe88846eb230a6cd19316bcbc5455e020b216
MRColor <mattiar11@gmail.com>
2023-07-04 19:32:07 +0200

    CHG - specific stteps for nightly or latest (stable)

--- abe-webapp/docker_run_stable.sh
+++ abe-webapp/docker_run_stable.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-docker build -t abe-webapp:latest .
-docker run -p 8000:8000 abe-webapp
+# docker build -t abe-webapp:latest .
+# docker run --name abeweb_stable -p 8000:8000 -d abe-webapp
+
+docker build -t mrcolorrain/kaboom:latest .\abe-webapp\ --no-cache
+docker run --name kaboom_abeweb_stable -p 8001:8000 -d mrcolorrain/kaboom:latest
--- docker_run_nightly.sh
+++ docker_run_nightly.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-docker build -t abe-webapp:nightly .
-docker run -p 8001:8000 abe-webapp
+#docker build -t abe-webapp:nightly .
+#docker run --name abeweb_nightly -p 8001:8000 -d abe-webapp
+
+docker build -t mrcolorrain/kaboom:nightly .\abe-webapp\ --no-cache
+docker run --name kaboom_abeweb_nightly -p 8001:8000 -d mrcolorrain/kaboom:nightly
